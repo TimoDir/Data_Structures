d3db3d26c96c4bc15b72ee10de63b5002b4d2a97
TimD <timothee.diroian@gmail.com>
2023-02-13 09:19:45 +0100

    Creation of a methode to set a next pointer to the node

--- Linear_Data_Structures/Nodes/Node.js
+++ Linear_Data_Structures/Nodes/Node.js
@@ -1,8 +1,17 @@
 class Node{
-    // The node recieved data and has a next and previous pointer;
     constructor(data){
         this.data = data;
         this.next = null;
         this.prev = null; 
+    }
+
+    // Creation of a Method to set a node to the next pointer;
+    setNextNode(node){
+        // we verify if the node parameter is an instance of the Node class or if is a null object
+        if(node instanceof Node || node === null){
+            this.next = node;
+        } else {
+            throw new Error('The parameter of the setNextNode method must be an instance of class Node. (or a null object)')
+        };
     }
 }
