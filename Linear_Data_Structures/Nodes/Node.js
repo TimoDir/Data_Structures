92be5fd63b83143f7b0238acdd6e38855e38cca0
TimD <timothee.diroian@gmail.com>
2023-02-13 09:13:44 +0100

    creation of the class Node + constructor

---
+++ Linear_Data_Structures/Nodes/Node.js
@@ -0,0 +1,8 @@
+class Node{
+    // The node recieved data and has a next and previous pointer;
+    constructor(data){
+        this.data = data;
+        this.next = null;
+        this.prev = null; 
+    }
+}
