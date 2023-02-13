b8c45922925bb78553e6b6ab9896024d2bce8493
TimD <timothee.diroian@gmail.com>
2023-02-13 09:56:46 +0100

    creation of the method addToHead and test with the console

--- Linear_Data_Structures/Singly_linked_lists/SinglyLinkedList.js
+++ Linear_Data_Structures/Singly_linked_lists/SinglyLinkedList.js
@@ -1,10 +1,31 @@
 const Node = require('../Nodes/Node');
 
 
-console.log(newNode);
-
 class SinglyLinkedList{
     constructor(){
         this.head = null;
     }
-}
+
+    addToHead(data){
+        //Put the data inside a node instance
+        const newHead = new Node(data);
+        //if there is no head to the list just add this newhead
+        if(this.head === null){
+            this.head = newHead;
+        } else {
+        //else we set the next node of this new head to the current head and replace the head with this new head
+            newHead.setNextNode(this.head);
+            this.head = newHead;
+        }
+    }
+};
+
+const Farm = new SinglyLinkedList();
+
+console.log(Farm)
+
+Farm.addToHead('cow');
+
+Farm.addToHead('Farmer');
+
+console.log(Farm)
