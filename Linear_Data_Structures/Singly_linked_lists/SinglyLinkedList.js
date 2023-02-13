abd55ecfc2a6850d66b45ecba445a39f2aa0bff9
TimD <timothee.diroian@gmail.com>
2023-02-13 10:18:30 +0100

    creation of the removeHead method

--- Linear_Data_Structures/Singly_linked_lists/SinglyLinkedList.js
+++ Linear_Data_Structures/Singly_linked_lists/SinglyLinkedList.js
@@ -4,7 +4,7 @@ const Node = require('../Nodes/Node');
 class SinglyLinkedList{
     constructor(){
         this.head = null;
-    }
+    };
 
     addToHead(data){
         const newHead = new Node(data);
@@ -16,34 +16,52 @@ class SinglyLinkedList{
         }
     };
 
+    removeHead(){
+        // if this.head === null there is nothing to remove.
+        if(!this.head){
+            return;
+        };
+        // we store the head we gonna remove to return is data as the end of the method. we set the head to the next pointer of the removedHead.
+        const removedHead = this.head;
+        this.head = removedHead.getNextNode();
+        return removedHead.data;
+    }
+
     printList(){
         let current = this.head;
 
-        // if this.head = null we return null
         if(!current){
             console.log(current);
             return
         };
 
-        // output variable who will store the data of the list
         let output = '<Head>\n';
-        // traverse recursively the list until there is no next node in the list
         while(current !== null){
             output += current.data + '\n';
             current = current.getNextNode();
         };
-
         output += '<Tail>';
+
         console.log(output)
-    }
+    };
 };
 
 const Farm = new SinglyLinkedList();
 
-Farm.printList();
+const noHead = Farm.removeHead();
+
+console.log('the removed head is ' + noHead + ' head \n');
 
 Farm.addToHead('cow');
 
 Farm.addToHead('Farmer');
+
+Farm.addToHead('chicken')
+
+Farm.printList();
+
+const fallingHead = Farm.removeHead();
+
+console.log('\nthe removed head is ' + fallingHead + ' head \n');
 
 Farm.printList();
