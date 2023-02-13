d540776895f4c87f8bb80ade2e1d70ab59d9d7af
TimD <timothee.diroian@gmail.com>
2023-02-13 09:21:15 +0100

    getNextNode method who return the next pointer

--- Linear_Data_Structures/Nodes/Node.js
+++ Linear_Data_Structures/Nodes/Node.js
@@ -5,13 +5,15 @@ class Node{
         this.prev = null; 
     }
 
-    // Creation of a Method to set a node to the next pointer;
     setNextNode(node){
-        // we verify if the node parameter is an instance of the Node class or if is a null object
         if(node instanceof Node || node === null){
             this.next = node;
         } else {
             throw new Error('The parameter of the setNextNode method must be an instance of class Node. (or a null object)')
         };
+    }
+
+    getNextNode(){
+        return this.next;
     }
 }
