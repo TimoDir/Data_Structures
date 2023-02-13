2cd63e1b706745758e13dc3a66ccb59cd9e32335
TimD <timothee.diroian@gmail.com>
2023-02-13 09:25:29 +0100

    creation of the methode setPrevNode and getPrevNode same way as the previous one but for the prev pointer

--- Linear_Data_Structures/Nodes/Node.js
+++ Linear_Data_Structures/Nodes/Node.js
@@ -16,4 +16,16 @@ class Node{
     getNextNode(){
         return this.next;
     }
-}
+    // same as the setNextNode methode but for the prev pointer
+    setPrevNode(node){
+        if(node instanceof Node || node === null){
+            this.prev = node;
+        } else {
+            throw new Error('The parameter of the setPrevNode method must be an instance of class Node. (or a null object)')
+        }
+    }
+
+    getPrevNode(){
+        return this.prev;
+    }
+};
